6ef022f05c095875fc38997cb6785089f078296a
afrizal0811 <afrizalmaulana0811@gmail.com>
2024-06-19 13:58:55 +0700

    remove unused file, add new file

---
+++ src/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import AboutPage from './pages/AboutPage'
+import CartPage from './pages/CartPage'
+import ContactPage from './pages/ContactPage'
+import HomePage from './pages/HomePage'
+import ShopPage from './pages/ShopPage'
+const Layout = () => (
+  <div>
+    <AboutPage />
+    <HomePage />
+    <CartPage />
+    <ShopPage />
+    <ContactPage />
+  </div>
+)
+
+export default Layout
