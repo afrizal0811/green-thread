81bf4a0779d4de6ebef97f1738d6f2dd3a3de60f
afrizal0811 <afrizalmaulana0811@gmail.com>
2024-08-14 15:15:40 +0700

    remove navigation in app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { BrowserRouter } from 'react-router-dom'
-import NavigationMenu from './components/navigation_menu/NavigationMenu'
 import RouteHandler from './RouteHandler'
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
-        <NavigationMenu />
         <RouteHandler />
       </BrowserRouter>
     </div>
