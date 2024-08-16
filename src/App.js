104c196046b7393b29fe597a3dbe7b283c01ef1a
afrizal0811 <afrizalmaulana0811@gmail.com>
2024-08-16 17:23:20 +0700

    add scroll to top

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter } from 'react-router-dom'
 import RouteHandler from './RouteHandler'
+import ScrollToTop from './utilities/scrollToTop'
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
+        <ScrollToTop />
         <RouteHandler />
       </BrowserRouter>
     </div>
